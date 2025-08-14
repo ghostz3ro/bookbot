6ead3ac94a4f15fbd8c685d11b18a85794286836
Thanos Sotiropoulos <thanos@customer.sfiabgr1.pop.starlinkisp.net>
2025-08-14 20:02:11 +0300

    now using sys.argv to take inputs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, count_of_characters, sort_on
+import sys
 
 # Reading the text of the book as a single string from a filepath input and returning it as one string.
 def get_book_text(path_to_file):
@@ -9,21 +10,26 @@ def get_book_text(path_to_file):
 
 
 # Setting the filepath and calling functions to get the text of the book and the number of words,
-filepath = "books/frankenstein.txt"
-book_text = get_book_text(filepath)
-word_count = get_num_words(book_text)
+# filepath = "books/frankenstein.txt"
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    filepath = sys.argv[1]
+    book_text = get_book_text(filepath)
+    word_count = get_num_words(book_text)
 
-# Calling the sort function and the character counting function in the same line
-character_appearances = sort_on(count_of_characters(book_text))
-#sorted_dict = sort_on(character_appearances)
+    # Calling the sort function and the character counting function in the same line
+    character_appearances = sort_on(count_of_characters(book_text))
+    #sorted_dict = sort_on(character_appearances)
 
-# Starting printing in accordance with the required report layout 
-print(f"Analysing book found at {filepath}")
-print("----------- Word Count ----------")
-print(f"Found {word_count} total words")
-print("--------- Character Count -------")
-for char in character_appearances: 
-    if char.isalpha() is True:
-        print(f"{char}: {character_appearances[char]}")
+    # Starting printing in accordance with the required report layout 
+    print(f"Analysing book found at {filepath}")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for char in character_appearances: 
+        if char.isalpha() is True:
+            print(f"{char}: {character_appearances[char]}")
 
-print("============= END ===============")
+    print("============= END ===============")
